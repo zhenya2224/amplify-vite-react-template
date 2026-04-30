1433c07e9189bbc80bbac91a7b27bd33524ce16d
zhenya2224 <zhenya2224@gmail.com>
2026-04-30 11:20:34 +0300

    added authenticator+

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { generateClient } from 'aws-amplify/data';
+import { useEffect, useState } from 'react';
+
+import type { Schema } from '../amplify/data/resource';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -14,7 +16,12 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: window.prompt('Todo content') });
+  }
+  const { signOut } = useAuthenticator();
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
 
   return (
@@ -23,7 +30,9 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -33,6 +42,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import outputs from '../amplify_outputs.json';
+import App from './App.tsx';
+import './index.css';
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>,
 );
